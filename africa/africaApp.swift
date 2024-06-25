7e20c330f090057cc0379efb1a4d8b64a312797e
BarisDilekci <bdilekci200@gmail.com>
2024-06-25 20:05:21 +0300

    Initial Commit

---
+++ africa/africaApp.swift
@@ -0,0 +1,17 @@
+//
+//  africaApp.swift
+//  africa
+//
+//  Created by Barış Dilekçi on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct africaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
