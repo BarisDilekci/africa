875cd1d01f7748d9d42988745841b26d4b54004d
BarisDilekci <bdilekci200@gmail.com>
2024-07-02 13:08:21 +0300

    fix

--- africa/Component/MapAnnotationView.swift
+++ africa/Component/MapAnnotationView.swift
@@ -18,16 +18,16 @@ struct MapAnnotationView: View {
 
   var body: some View {
     ZStack {
-      Circle()
-        .fill(Color.accentColor)
-        .frame(width: 54, height: 54, alignment: .center)
-      
-      Circle()
-        .stroke(Color.accentColor, lineWidth: 2)
-        .frame(width: 52, height: 52, alignment: .center)
-        .scaleEffect(1 + CGFloat(animation))
-        .opacity(1 - animation)
-      
+        Circle()
+          .fill(Color.accentColor)
+          .frame(width: 54, height: 54, alignment: .center)
+        
+        Circle()
+          .stroke(Color.accentColor, lineWidth: 2)
+          .frame(width: 52, height: 52, alignment: .center)
+          .scaleEffect(1 + CGFloat(animation))
+          .opacity(1 - animation)
+        
       Image(location.image)
         .resizable()
         .scaledToFit()
